574b87f5e9e11c2991edfd22b88e3f1fa6ee75ee
francman <frankmanu500@gmail.com>
2018-04-18 18:02:11 -0400

    updated for RX_1 settings

--- ADAR1000_Config.py
+++ ADAR1000_Config.py
@@ -1,3 +1,15 @@
 #This script configures registers for the ADAR1000 
-
-from SDP_driver_new import *
+"""
+#Initializing ADAR1000 RX_1 for signal input.
+            mySdp.write_spi(0x2000, 0x81)     #Reset the whole board
+            mySdp.write_spi(0x2000, 0x18)     #Configure board for SPI communication
+            mySdp.write_spi(0x2400, 0x55)     #Adjust LDOs
+            mySdp.write_spi(0x2038, 0x60)     #Select SPI for channel settings
+            mySdp.write_spi(0x202e, 0x7f)     #Enable LNA
+            mySdp.write_spi(0x2034, 0x08)     #Set RX LNA bias to 8
+            mySdp.write_spi(0x2035, 0x16)     #Set RX VGA bias to 2
+            mySdp.write_spi(0x2031, 0x20)     
+            mySdp.write_spi(0x2010, 0xff)    
+            mySdp.write_spi(0x2014, 0x36)     
+            mySdp.write_spi(0x2015, 0x36)     
+            mySdp.write_spi(0x2028, 0x01)     
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Date: 04/12/2018
 # Purpose: This is the main file for the system
 
-from SDP_driver_new import * 
+from SDP_driver_new import *
 
 def main():
     # List of all Hardware IDs I want to look through to see if a board matches.
@@ -21,22 +21,22 @@ def main():
         print 'Board Description =', mySdp.description
 
     else:
-        print '\n\tNo connection made... handle is a dummy'
+        print '\n\tNo connection made...'
 
-    #Initializing ADAR1000 RX_1 for signal input.
-    mySdp.write_spi(0x00, 0x81)     #Reset the whole board
-    mySdp.write_spi(0x00, 0x18)     #Configure board for SPI communication
-    mySdp.write_spi(0x400, 0x55)    #Adjust LDOs
-    mySdp.write_spi(0x38, 0x60)     #Select SPI for channel settings
-    mySdp.write_spi(0x2e, 0x7f)     #Enable LNA
-    mySdp.write_spi(0x34, 0x08)     #Set RX LNA bias to 8
-    mySdp.write_spi(0x35, 0x16)     #Set RX VGA bias to 2
-    mySdp.write_spi(0x31, 0x20)     #
-    mySdp.write_spi(0x10, 0xff)     #Set VGA gain to max
-    mySdp.write_spi(0x14, 0x36)     #Set CH1 Vec mod I-input to +, magnitude 16
-    mySdp.write_spi(0x15, 0x36)     #Vec mod Q-input to +, magnitude ‘15’; these two together set phase to 45 deg
-    mySdp.write_spi(0x28, 0x01)     #
-    
+    if mySdp.connected:
+        #Initializing ADAR1000 RX_1 for signal input.
+        mySdp.write_spi(0x0000, 0x81)     #Reset the whole board
+        mySdp.write_spi(0x0000, 0x18)     #Configure board for SPI communication
+        mySdp.write_spi(0x0400, 0x55)     #Adjust LDOs
+        mySdp.write_spi(0x0038, 0x60)     #Select SPI for channel settings
+        mySdp.write_spi(0x002e, 0x7f)     #Enable LNA
+        mySdp.write_spi(0x0034, 0x08)     #Set RX LNA bias to 8
+        mySdp.write_spi(0x0035, 0x16)     #Set RX VGA bias to 2
+        mySdp.write_spi(0x0031, 0x20)     
+        mySdp.write_spi(0x0010, 0xff)    
+        mySdp.write_spi(0x0014, 0x36)     
+        mySdp.write_spi(0x0015, 0x36)     
+        mySdp.write_spi(0x0028, 0x01) 
         
 if __name__ == '__main__':
     main()
